39237835bda45f4326376c8cdb078a282a0f6ccd
Frost <fizry.r@tindogroup.com>
2020-10-15 14:39:02 +0800

    Update

--- app_test2.rb
+++ app_test2.rb
@@ -44,13 +44,24 @@ get "/runDelete" do
 	delete_template
 end
 
+post "/runDelete" do
+	$store_id = params[:store_id]
+	delete
+	back_to_index
+end
+
+
+#About template is retrieved and displayed
+get "/runAbout" do
+        about_template
+end
 
 def mysql_client
 	puts "LOADING BOOT PROGRAM!"
 	table_arr = []
 
 	#Query results from safeEntry
-	results = $client.query("SELECT * FROM safeEntry ORDER BY crowd_level ASC;")
+	results = $client.query("SELECT * FROM safeEntry ORDER BY store_id ASC;")
 
 	#Store each result entry into dictionary before being stored in table_arr
 	results.each do |row|
@@ -106,25 +117,28 @@ def insert
 	puts "INSERT SEQUENCE LOADED!"
 end
 
+def delete
+	puts "LOADING DELETE SEQUENCE!"
+
+	results = $client.query("SELECT store_id FROM safeEntry;")
+	
+	if $store_id.to_s.empty? != TRUE
+		$client.query("DELETE FROM safeEntry WHERE store_id=" + $store_id.to_s + ";")
+		puts "DELETE STATEMENT HAS BEEN QUERIED!"
+	end
+	
+	
+	puts "DELETE SEQUENCE LOADED!"
+end
+
 def index_template
         "
         <html>
                 <head>
+			<meta charset='utf-8'>
+			<link rel='stylesheet' type='text/css' href='/application.css'/>
+			<title>Safe Entry Management Portal</title>
 		</head>
-                <style>
-			body {background-color: #FFE4E1; margin: 0; padding: 0; font-family: Times New Roman;}
-			table, td, tr, th{border: 1px solid black; width: 550px; text-align: center;}
-			.sg_gov {width: 100%; overflow:auto; background-color: #DCDCDC; font-size: 16px;}
-			.sg_gov p {padding-left: 90px; padding-top: 5px;}
-			.safeEntry_img img {position: absolute; background-color: #FFFFFF; margin-top: 15px; margin-left: 10px; width: 200px; height: 40; }
-			nav {width: 100%; background: #FFFFFF; overflow: auto;}
-			ul {list-style-type: none; margin: 0 0 0 150px; padding: 0;}
-			li {float: right;}
-			li a {display: block; width: 100px; padding: 20px 15px; text-align: center; color: black; font-size: 20px; text-decoration: none;}
-			li a:hover {background: #D3D3D3; color: white; transition: 0.5s;}
-			section {margin-left: 15px; font-size: 20px;}
-
-                </style>
                 <body>
 			<header>
 				<div class='sg_gov'>
@@ -132,7 +146,7 @@ def index_template
 				</div>
 				<div class='safeEntry_directory'>
 					<nav class='nav_bar'>
-						<a class='safeEntry_img' href='#'>
+						<a class='safeEntry_img' href='/index'>
 							<img src='https://www.ndi-api.gov.sg/assets/img/safe-entry/SafeEntry_logo_inline.png' alt='Safe Entry Logo'/>
                                                 </a>
 						<ul>
@@ -145,7 +159,7 @@ def index_template
 				</div>
 			</header>
 			<section>
-				<p>Welcome to the Safe Entry Management Portal! Here you'll be able to access the crowd levels of various shopping malls and buildings. Simply click on the <b>Refresh</b> to update the page with new records. Click <b>Insert</b> to add a new building to the list! And click <b>Delete</b> to remove a building from the list!<br></br><b><i> #LET'S DO OUR PART!</i></b><br><b><i> #SGUNITED!</i></b></br></p>
+				<p>Welcome to the Safe Entry Management Portal(SEMP)! Here you'll be able to access the crowd levels of various shopping malls and buildings. Simply click on the <b>Refresh</b> to update the page with new records. Click <b>Insert</b> to add a new building to the list! And click <b>Delete</b> to remove a building from the list!<br></br><b><i> #LET'S DO OUR PART!</i></b><br><b><i> #SGUNITED!</i></b></br></p>
 				<br>
 				#{$xm}
 				<br>
@@ -160,17 +174,43 @@ end
 def insert_template
 	"
 	<html>
+		<head>
+			<meta charset='utf-8'>
+                        <link rel='stylesheet' type='text/css' href='/application.css'/>
+                        <title>SEMP - Insert Page</title>
+
+		</head>
 		<body>
-			<h1>Welcome to Safe Entry Insert Page</h1>
-			<p>Please enter the name of the building and click [Submit] when you are ready!</p>
-			<form method='post' action='/runInsert'>
-				Location Name: <input type='text' name='location'><br></br>
-				Limit: <input type='text' name='limit'><br></br>
-				<button type='submit' value='Submit'>Submit</button>
-			</form>
-			<a href='/index'>
-				<input type='submit' value='Back'/>
-			</a>
+			<header>
+				<div class='sg_gov'>
+                                        <p>A Singapore Governement Agency Website</p>
+                                </div>
+                                <div class='safeEntry_directory'>
+                                        <nav class='nav_bar'>
+                                                <a class='safeEntry_img' href='/index'>
+                                                        <img src='https://www.ndi-api.gov.sg/assets/img/safe-entry/SafeEntry_logo_inline.png' alt='Safe Entry Logo'/>
+                                                </a>
+                                                <ul>
+                                                        <li><a href='/runDelete'>Delete</a></li>
+                                                        <li><a href='/runInsert'>Insert</a></li>
+                                                        <li><a href='/runUpdate'>Refresh</a></li>
+                                                        <li><a href='/runAbout'>About</a></li>
+                                                </ul>
+                                        </nav>
+                                </div>
+                        </header>
+			<section>
+				<h1>Welcome to Safe Entry Insert Page</h1>
+				<p>Please state the name of the store followed by the crowd limit!</p>
+				<form method='post' action='/runInsert'>
+					Store Name: <input type='text' name='location'><br></br>
+					Crowd Limits: <input type='text' name='limit'><br></br>					
+					<button type='submit' value='Submit'>Submit</button>
+					<a href='/index'>
+						<input type='button' value='Back'>
+					</a>
+				</form>
+			</section>
 		</body>
 	</html>
 	"
@@ -179,26 +219,141 @@ end
 def delete_template
 	"
 	<html>
+		<head>
+                        <meta charset='utf-8'>
+                        <link rel='stylesheet' type='text/css' href='/application.css'/>
+                        <title>SEMP - Delete Page</title>
+
+                </head>
 		<body>
-			<h1>Welcome to Safe Entry Delete Page</h1>
-			<p style='color:red'>I'm Sorry This Page Is Currently Under Construction! We Apologize For Any Inconveniences Caused</p><br>
-			<a href='/index'>
-				<input type='submit' value='Back'/>
-			</a>
-			</br>
+                       <header>
+                                <div class='sg_gov'>
+                                        <p>A Singapore Governement Agency Website</p>
+                                </div>
+                                <div class='safeEntry_directory'>
+                                        <nav class='nav_bar'>
+                                                <a class='safeEntry_img' href='/index'>
+                                                        <img src='https://www.ndi-api.gov.sg/assets/img/safe-entry/SafeEntry_logo_inline.png' alt='Safe Entry Logo'/>
+                                                </a>
+                                                <ul>
+                                                        <li><a href='/runDelete'>Delete</a></li>
+                                                        <li><a href='/runInsert'>Insert</a></li>
+                                                        <li><a href='/runUpdate'>Refresh</a></li>
+                                                        <li><a href='/runAbout'>About</a></li>
+                                                </ul>
+                                        </nav>
+                                </div>
+                        </header>
+			<section>
+				<h2>Welcome to Safe Entry Delete Page</h2>
+                                <p>Refer to the table below and key in the respective Store ID in the input box!</p>
+				#{$xm}
+				<br>
+                                <form method='post' action='/runDelete'>
+                                        Store ID: <input type='text' name='store_id'><br></br></br>
+                                        <button type='submit' value='Submit'>Submit</button>
+                                        <a href='/index'>
+                                                <input type='button' value='Back'>
+                                        </a>
+                                </form>
+			</section>
 		</body>
 	</html>
 	"
 end
 
+def about_template
+        "
+        <html>
+                 <head>
+                        <meta charset='utf-8'>
+                        <link rel='stylesheet' type='text/css' href='/application.css'/>
+                        <title>SEMP - Insert Page</title>
+
+                </head>
+                <body>
+                        <header>
+                                <div class='sg_gov'>
+                                        <p>A Singapore Governement Agency Website</p>
+                                </div>
+                                <div class='safeEntry_directory'>
+                                        <nav class='nav_bar'>
+                                                <a class='safeEntry_img' href='/index'>
+                                                        <img src='https://www.ndi-api.gov.sg/assets/img/safe-entry/SafeEntry_logo_inline.png' alt='Safe Entry Logo'/>
+                                                </a>
+                                                <ul>
+                                                        <li><a href='/runDelete'>Delete</a></li>
+                                                        <li><a href='/runInsert'>Insert</a></li>
+                                                        <li><a href='/runUpdate'>Refresh</a></li>
+                                                        <li><a href='/runAbout'>About</a></li>
+                                                </ul>
+                                        </nav>
+                                </div>
+                        </header>
+                        <section>
+				<h1>About</h1>
+				<br>
+                                <h3>What is Safe Entry?</h3>
+				<p>SafeEntry is a national digital check-in system that logs the NRIC/FINs and mobile numbers of individuals visiting hotspots, workplaces of essential services, as well as selected public venues to prevent and control the transmission of COVID-19 through activities such as contact tracing and identification of COVID-19 clusters. Individuals can choose to check in/out from SafeEntry at entry/exit points using any of the following methods:
+(a) Scan QR code: Use the SingPass Mobile app, TraceTogether app, your mobile phone’s camera function or a recommended QR scanner app to scan a QR code and submit your personal particulars; or
+(b) Scan ID card: Present an identification card barcode (e.g. NRIC, Passion card, Pioneer Generation card, Merdeka Generation card, driver’s licence, Transitlink concession card, student pass, work permit, SingPass Mobile app, TraceTogether app) to be scanned by staff; or
+(c) Select from a list of nearby locations: Use the SingPass Mobile app’s ‘SafeEntry Check-In’ function to select a location and check in.</p>
+				</br>
+                                <h3>Why is Safe Entry being deployed to more places?</h3>
+                                <p>As more activities and services gradually resume following the circuit breaker period, it is important that efforts to prevent and control the transmission of COVID-19 such as contact tracing and identification of COVID-19 clusters can be done quickly to limit the risk of further community transmission. SafeEntry helps support and quicken these efforts to prevent and control the incidence or transmission of COVID-19 as it provides authorities with a record of individuals who enter and exit places. The records will reduce the time needed to identify potential close contacts of COVID-19 patients and potential COVID-19 clusters. This is important so that we can continue advancing towards fewer restrictions on our movements, and our daily lives</p>
+				<br>
+				<h3>Why do we need to use SafeEntry instead of existing vendor management systems?</h3>
+				<p>The use of SafeEntry is mandatory because a common system used by all establishments would allow data to be made available to MOH quickly, so as to facilitate efforts to prevent and control the transmission of COVID-19 through activities such as contact tracing and identification of COVID-19 clusters. SafeEntry allows information of visitors and employees who may have come into contact with COVID-19 cases to be sent to the authorities automatically. Contact data collected by SafeEntry is only used by authorised personnel, and stringent measures are in place to safeguard the data in accordance with the Government’s data security standards.</p>
+				</br>
+                                <h3>Can I use an alternative visitor management system instead of SafeEntry?</h3>
+                                <p>From 12 May onwards, businesses are required to use SafeEntry to collect entry information of employees and visitors on their premises. Businesses that need to retain the use of their current system for the collection of data that are not required in the SafeEntry system (e.g. purpose of visit, employee’s ID number) are required to implement SafeEntry on top of their existing system. To cater to visitors who are not able to scan QR codes or do not have their identification cards with them, businesses are advised to also assist individuals to check in through the manual entry function in SafeEntry using their NRIC, or the webform in SafeEntry with the QR code using any available device.</p>
+                                <br>
+                                <h3>What happens if safeEntry breaks down? Is there flexibility in the enforcement of SafeEntry?</h3>
+				<p>Businesses should tap on the alternate mode of SafeEntry in the unlikely event that their preferred mode breaks down, i.e. use SafeEntry QR as back-up if SafeEntry NRIC is the preferred mode, and vice versa. Businesses may do so by setting up the alternate mode at <a href='https://www.SafeEntry.gov.sg'/>https://www.SafeEntry.gov.sg</a>. SafeEntry does not recommend hard copy form filling as a back-up.
+
+The common use of SafeEntry by all establishments would allow data to be automatically sent to MOH. Hard copy recording of particulars would present a gap in this automated process and affect the contact tracing process.</p>
+                                </br>
+                        </section>
+                </body>
+        </html>
+        "
+end
+
+
 def back_to_index
 	"
 	<html>
+		 <head>
+                        <meta charset='utf-8'>
+                        <link rel='stylesheet' type='text/css' href='/application.css'/>
+                        <title>SEMP - Insert Page</title>
+
+                </head>
 		<body>
-			<p> Click the [Back] button to return to the index page.<br></br>
-			<a href='/index'>
-				<input type='submit' value='Back'/>
-			</a>
+                        <header>
+                                <div class='sg_gov'>
+                                        <p>A Singapore Governement Agency Website</p>
+                                </div>
+                                <div class='safeEntry_directory'>
+                                        <nav class='nav_bar'>
+                                                <a class='safeEntry_img' href='/index'>
+                                                        <img src='https://www.ndi-api.gov.sg/assets/img/safe-entry/SafeEntry_logo_inline.png' alt='Safe Entry Logo'/>
+                                                </a>
+                                                <ul>
+                                                        <li><a href='/runDelete'>Delete</a></li>
+                                                        <li><a href='/runInsert'>Insert</a></li>
+                                                        <li><a href='/runUpdate'>Refresh</a></li>
+                                                        <li><a href='/runAbout'>About</a></li>
+                                                </ul>
+                                        </nav>
+                                </div>
+                        </header>
+			<section>
+				<p> Click the [Back] button to return to the index page.<br></br>
+				<a href='/index'>
+					<input type='submit' value='Back'/>
+				</a>
+			</section>
 		</body>
 	</html>
 	"
